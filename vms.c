c4b8c05cd28eeb0aedd910be9eb63445b5eb7ad7
Trayvally <1424025383@qq.com>
2020-03-01 20:50:42 +0800

    售货机

--- vms.c
+++ vms.c
@@ -10,6 +10,7 @@ int price[5] = {10, 6, 5, 8, 7};
 int stock[5] = {5, 1, 2, 1, 9};
 char lights_up[5] = {' ',' ',' ',' ',' '};
 int pwd = 1110;
+int income;
 
 void mainPage()
 {
@@ -42,6 +43,8 @@ int main()
 	int choice3;
 	int choice4;
 	int choice9;
+	int choice9_3;
+	int choice9_4;
 
     mainPage();
     while (1)
@@ -97,10 +100,6 @@ int main()
 			}*/
 
             break;
-
-
-
-
 
         case 3:
 			printf("(3) Which product button would you like to press?\n");
@@ -165,7 +164,7 @@ int main()
 			scanf("%d",&pwd);
 			if (pwd != 1110)
 			{
-				printf("Incorrect code!");
+				printf("Incorrect code!\n");
 			}else
 			{
 				printf("Correct code!\n");
@@ -184,10 +183,89 @@ int main()
 				}
 				switch (choice9)
 				{
-				case 1:money = money + 1;printf("You have inserted ¥1.\n");break;
-				case 2:money = money + 2;printf("You have inserted ¥2.\n");break;
-				case 3:money = money + 5;printf("You have inserted ¥5.\n");break;
-				case 4:money = money + 10;printf("You have inserted ¥10.\n");break;
+				case 1:
+					printf("(9-1)Machine status\n");
+					printf("Amount of revenue: ¥%d\n",income); 
+					printf("Amount of inserted coins: ¥%d\n",money);
+					printf("A. Juice ($10) (%d left)\n",stock[0]); 
+					printf("B. Cola ($6) (%d left)\n",stock[1]); 
+					printf("C. Tea ($5) (%d left)\n",stock[2]); 
+					printf("D. Water ($8) (%d left)\n",stock[3]); 
+					printf("E. Coffee ($7) (%d left)\n",stock[4]); 
+					break;
+				case 2:
+					printf("(9-2)All money is being withdrawn\n");
+					printf("¥%d is withdrawn\n",income + money);
+					break;
+				case 3:
+					printf("(9-3) Which product would you like to refill?\n");
+					printf("1. A\n");
+					printf("2. B\n");
+					printf("3. C\n");
+					printf("4. D\n");
+					printf("5. E\n");
+					printf("0. Go back\n");
+					printf("Your choice:\n");
+					scanf("%d",&choice9_3);
+					if (choice9_3 == 0)
+					{
+						break;
+					}
+					switch (choice9_3)
+					{
+					case 1:
+						printf("You have refilled product A to full.\n");
+						break;
+					case 2:
+						printf("You have refilled product B to full.\n");
+						break;
+					case 3:
+						printf("You have refilled product C to full.\n");
+						break;
+					case 4:
+						printf("You have refilled product D to full.\n");
+						break;
+					case 5:
+						printf("You have refilled product E to full.\n");
+						break;
+					default:
+						break;
+					}
+					break;
+				case 4:
+					printf("(9-4) Which product would you like to change?\n");
+					printf("1. A\n");
+					printf("2. B\n");
+					printf("3. C\n");
+					printf("4. D\n");
+					printf("5. E\n");
+					printf("0. Go back\n");
+					printf("Your choice:\n");
+					scanf("%d",&choice9_4);
+					if (choice9_4 == 0)
+					{
+						break;
+					}
+					switch (choice9_4)
+					{
+					case 1:
+						printf("You are changing product A.\n");
+						break;
+					case 2:
+						printf("You are changing product B.\n");
+						break;
+					case 3:
+						printf("You are changing product C.\n");
+						break;
+					case 4:
+						printf("You are changing product D.\n");
+						break;
+					case 5:
+						printf("You are changing product E.\n");
+						break;
+					default:
+						break;
+					}
 				default:printf("Invalid choice!\n");break;
 				}
 				mainPage();
